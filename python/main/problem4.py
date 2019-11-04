91731417bfce3d80aff4015eac7bc3ce0876a6ec
Olivier Truong <olivier@nadeva.fr>
2019-11-04 08:21:08 +0100

    housekeeping

--- python/main/problem4.py
+++ python/main/problem4.py
@@ -1,8 +1,8 @@
 def get_problem4():
-    return find_largest_palindrome_from_product_of_2_3digits_number()
+    return find_largest_palindrome_from_product_of_3digits_number()
 
 
-def find_largest_palindrome_from_product_of_2_3digits_number():
+def find_largest_palindrome_from_product_of_3digits_number():
     max_palindrome = 0
     for i in range(100, 1000):
         for j in range(i, 1000):
